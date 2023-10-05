0325c8966c8ca1c5bce8c602c67c4e140902abc7
sarahannata <sarahannata@gmail.com>
2023-10-05 07:21:16 +0700

    Finishing Praktikum 1

---
+++ app/src/main/java/com/example/pampraktikum1/activity_frame_layout.kt
@@ -0,0 +1,11 @@
+package com.example.pampraktikum1
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class activity_frame_layout : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_frame_layout)
+    }
+}
---
+++ app/src/main/java/com/example/pampraktikum1/table_layout.kt
@@ -0,0 +1,11 @@
+package com.example.pampraktikum1
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class table_layout : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_table_layout)
+    }
+}
